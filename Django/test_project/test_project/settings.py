5ed0e496391b114cd67bdc7c5757d9ecdb09995c
rchrdfrrr <richardferrer1792@gmail.com>
2026-02-19 21:53:30 +0800

    revised files

--- Django/test_project/test_project/settings.py
+++ Django/test_project/test_project/settings.py
@@ -24,17 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
+# # Take environment variables from .env file
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-7ngz0m5-7*&ox#1ozx56-fxum%b225ljl-h#c6!hg%1r(#+$-^'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1']
 
