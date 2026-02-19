371e69e875660e645efe4c981f7b306de60f440b
rchrdfrrr <richardferrer1792@gmail.com>
2026-02-19 21:51:49 +0800

    revised files

--- Django/test_project/test_project/settings.py
+++ Django/test_project/test_project/settings.py
@@ -28,7 +28,8 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
